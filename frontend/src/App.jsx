1fa794f9d5923ad848ee0e9252f51a90ca935ad3
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-02-03 13:44:37 +0300

    Slider and Categories added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,20 @@
 import './App.css'
-import Footer from './components/Footer'
-import Header from './components/Header'
-import Policy from './components/Policy'
+import Categories from './components/Categories/Categories'
+import Footer from './components/Layout/Footer/Footer'
+import Header from './components/Layout/Header/Header'
+import Policy from './components/Layout/Policy/Policy'
+import Slider from './components/Slider/Slider'
 
 function App() {
 
   return (
-    <>
+    <div>
       <Header/>
+      <Slider/>
+      <Categories/>
       <Policy/>
       <Footer/>
-  
-    </>
+    </div>
   )
 }
 
