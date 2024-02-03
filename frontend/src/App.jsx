da2dd8809a5fb8f4b760cb6edd23bb1c6aa8aebb
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-02-03 12:46:05 +0300

    Footer added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import Footer from './components/Footer'
 import Header from './components/Header'
+import Policy from './components/Policy'
 
 function App() {
 
   return (
     <>
       <Header/>
+      <Policy/>
+      <Footer/>
+  
     </>
   )
 }
