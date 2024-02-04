2c477b5e98ddb69ab37665ef8e9366f58d69c28c
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-02-04 19:55:11 +0300

    slider library added

--- frontend/src/components/Slider/SliderItem.jsx
+++ frontend/src/components/Slider/SliderItem.jsx
@@ -1,9 +1,9 @@
-
-const SliderItem = () => {
+import Proptypes from "prop-types";
+const SliderItem = ({imageSrc}) => {
   return (
     <div className="slider-item fade">
     <div className="slider-image">
-      <img src="img/slider/slider1.jpg" className="img-fluid" alt="" />
+      <img src={imageSrc} className="img-fluid" alt="" />
     </div>
     <div className="container">
       <p className="slider-title">SUMMER 2022</p>
@@ -17,3 +17,7 @@ const SliderItem = () => {
 }
 
 export default SliderItem
+
+SliderItem.propTypes = {
+  imageSrc: Proptypes.string
+};
