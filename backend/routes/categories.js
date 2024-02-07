2b675fbd27913a684e6fa43beea0e615505f06f0
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-02-07 12:40:12 +0300

    create category and real all

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/Category.js");
 
-//bring all categories
+//create a new category
+router.post("/", async (req, res) => {
+  try {
+    const { name, img } = req.body; //kullanıcının girdiği veri buradan alınır
+    const newCategory = new Category({ name, img });
+    await newCategory.save();
+
+    res.status(201).json(newCategory); //başarılı olursa cevap dön 201 yeni oluşma başarılı kodu
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//bring all categories(Read-All)
 router.get("/", async (req, res) => {
-  res.send("categories were brought");
+  try {
+    const categories = await Category.find();
+    res.status(200).json(categories);
+  } catch (error) {
+    res.status(500).json({error:"Server error."})
+  }
 });
 
 module.exports = router;
