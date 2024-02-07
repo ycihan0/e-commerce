41db77847033343f879ca5fdc4f5239b0a99750c
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-02-07 13:51:56 +0300

    update category

--- backend/routes/categories.js
+++ backend/routes/categories.js
@@ -18,10 +18,50 @@ router.post("/", async (req, res) => {
 //bring all categories(Read-All)
 router.get("/", async (req, res) => {
   try {
-    const categories = await Category.find();
+    const categories = await Category.find(); //mongoose methodu find ile içindeki verileri alma
     res.status(200).json(categories);
   } catch (error) {
-    res.status(500).json({error:"Server error."})
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+//bring one category (Read-Sigle)
+router.get("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId; //req.params={{url}}/categories/
+    try {
+      const category = await Category.findById(categoryId);
+      res.status(200).json(category);
+    } catch (error) {
+      console.log(error);
+      res.status(404).json({ error: "Category not found." });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+//category update
+router.put("/:categoryId", async (req, res) => {
+  try {
+    const categoryId = req.params.categoryId;
+    const updates = req.body;
+    const existingCategory = await Category.findById(categoryId);
+    if (!existingCategory) {
+      return res.status(404).json({ error: "Category not found." });
+    }
+    //Eğer Idyi bulamazsan hata dön;
+
+    const updatedCategory = await Category.findByIdAndUpdate(
+      categoryId,
+      updates,
+      { new: true } //güncellenmiş değeri gönder
+    );
+    res.status(200).json(updatedCategory);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "error: Server error." });
   }
 });
 
