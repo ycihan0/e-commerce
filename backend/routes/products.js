ccb382e1d27bea3e917060d0a57b3b4f904efd99
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-02-08 15:46:03 +0300

    product.js created

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,81 @@
 const express = require("express");
 const router = express.Router();
+const Product = require("../models/Product.js");
 
-//bring all products
+//create a new product
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+//bring all products (Read-All)
 router.get("/", async (req, res) => {
-  res.send("products were brought");
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+//bring one Product (Read-Sigle)
+router.get("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
+});
+
+//product update
+router.put("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const updates = req.body;
+    const existingProduct = await Product.findById(productId);
+    if (!existingProduct) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+
+    const updatedProduct = await Product.findByIdAndUpdate(productId, updates, {
+      new: true,
+    });
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "error: Server error." });
+  }
+});
+
+//product delete
+router.delete("/:productId", async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    const deletedProduct = await Product.findByIdAndDelete(productId);
+
+    if (!deletedProduct) {
+      return res.status(404).json({ error: "Product not found." });
+    }
+
+    res.status(200).json(deletedProduct);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error." });
+  }
 });
 
 module.exports = router;
