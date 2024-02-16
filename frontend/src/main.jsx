0f9296a158cd66e587437c4bc0efdfcff6f18167
Cihan Yuksel <cihan_yuksel26@hotmail.com>
2024-02-16 20:18:24 +0300

    scrollToTop added

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,11 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./index.css";
 import { Layout } from "./layouts/Layout.jsx";
-
-
+import ScrollToTop from "./components/ScrollToTop.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
+    <ScrollToTop />
     <CartProvider>
       <Layout>
         <App />
